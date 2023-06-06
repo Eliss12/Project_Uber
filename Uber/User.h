fea4259e5f6912ec338cecd899b5046dac8aa48e
Eliss12 <elis.shukry@gmail.com>
2023-06-06 22:49:38 +0300

    Base class User.h

---
+++ Uber/User.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "MyString.h"
+
+class User
+{
+protected:
+	MyString _type = "";
+	MyString _userName = "";
+	MyString _password = "";
+	MyString _firstName = "";
+	MyString _lastName = "";
+
+public:
+	User() = default;
+	User(const MyString& type, const MyString& userName, const MyString& password, const MyString& firstName, const MyString& lastName);
+
+	const MyString& getType() const;
+	const MyString& getUserName() const;
+	const MyString& getPassword() const;
+	const MyString& getFirstName() const;
+	const MyString& getLastName() const;
+
+	void setType(const MyString& type);
+	void setUserName(const MyString& userName);
+	void setPassword(const MyString& password);
+	void setFirstName(const MyString& firstName);
+	void setLastName(const MyString& lastName);
+	
+};
